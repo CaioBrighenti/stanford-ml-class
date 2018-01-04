Fixed gradient descent on python ex1

ex1/Python/ex1.py
@@ -23,9 +23,9 @@ y = data.iloc[:, cols-1:cols]
 # dataframes -> matrices
 X = np.matrix(X.values)
 y = np.matrix(y.values)
-theta = np.matrix(np.array([0,0])).T
+theta = np.matrix(np.array([0,0]))
 
-ComputeCost(X, y, theta)
+#computeCost(X, y, theta)
 
 # prepare variables for gradient descen
 alpha = 0.01
@@ -33,13 +33,11 @@ num_iters = 1000
 
 # perform gradient descent
 g, cost = gradientDescent(X, y, theta, alpha, num_iters)
-g = g.T
 
-# visualize linear regression
+#visualize data
 x = np.linspace(data.Population.min(), data.Population.max(), 100)
 f = g[0, 0] + (g[0, 1] * x)
 
-# plot best fit line
 fig, ax = plt.subplots(figsize=(12,8))
 ax.plot(x, f, 'r', label='Prediction')
 ax.scatter(data.Population, data.Profit, label='Traning Data')
@@ -48,7 +46,6 @@ ax.set_xlabel('Population')
 ax.set_ylabel('Profit')
 ax.set_title('Predicted Profit vs. Population Size')
 
-# plot gradient descent as a function of cost and # of iterations
 fig, ax = plt.subplots(figsize=(12,8))
 ax.plot(np.arange(num_iters), cost, 'r')
 ax.set_xlabel('Iterations')
@@ -56,25 +53,24 @@ ax.set_ylabel('Cost')
 ax.set_title('Error vs. Training Epoch')
 
 # function to compute J
-def ComputeCost(X, y, theta):
+def computeCost(X, y, theta):
     m = len(X)
-    predict = X * theta
+    predict = X * theta.T
     errors = np.power((predict - y), 2)
     return np.sum(errors) / (2*m)
 
 def gradientDescent(X, y, theta, alpha, num_iters):
-    m = len(y)
     J_history = np.zeros(num_iters)
     temp_theta = np.matrix(np.zeros(theta.shape))
 
     for i in range(num_iters):
-        errors = (X * theta) - y
+        errors = (X * theta.T) - y
 
         for j in range(X.shape[1]):
             term = np.multiply(errors, X[:, j])
-            temp_theta[j, 0] = theta[j,0] - ((alpha / m) * np.sum(term))
+            temp_theta[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term))
 
         theta = temp_theta
-        J_history[i] = ComputeCost(X, y, theta)
+        J_history[i] = computeCost(X, y, theta)
 
     return theta, J_history