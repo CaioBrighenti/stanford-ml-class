21dcb23aad548099c14db6bcf9df85a00ca8ac2f
unknown <caio.brig@gmail.com>
2018-01-05 11:13:18 -0200

    Finished multivariate linear regression in python

--- ex1/Python/ex1_mmulti.py
+++ ex1/Python/ex1_mmulti.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 # read data from files
 path = os.getcwd() + '\ex1\Python\ex1data2.txt'
 data = pd.read_csv(path, header=None, names=['Area', 'Rooms', 'Cost'])
+# normalize features to around -1 < x < 1 range
 data = (data - data.mean()) / data.std()
 data.head(10)
 data.describe()
@@ -26,10 +27,25 @@ theta = np.matrix(np.array([0,0,0]))
 alpha = 0.01
 num_iters = 1000
 
-# normalize features
+# Replaced with built in dataframe mean and std functions
 #X = featureNormalize(X)
+
 # perform gradient descent
 theta, cost = gradientDescent(X, y, theta, alpha, num_iters)
+theta_normal = normalEqn(X, y)
+
+# plot cost history to ensure gradient descent is working
+fig, ax = plt.subplots(figsize=(12,8))
+ax.plot(np.arange(num_iters), cost, 'r')
+ax.set_xlabel('Iterations')
+ax.set_ylabel('Cost')
+
+# function to compute J
+def computeCost(X, y, theta):
+    m = len(X)
+    predict = X * theta.T
+    errors = np.power((predict - y), 2)
+    return np.sum(errors) / (2*m)
 
 def gradientDescent(X, y, theta, alpha, num_iters):
     J_history = np.zeros(num_iters)
@@ -45,7 +61,7 @@ def gradientDescent(X, y, theta, alpha, num_iters):
             temp_theta[0, j] = theta[0, j] - alpha / len(X) * np.sum(term)
 
         theta = temp_theta
-        #J_history[i] = computeCost(X, y, theta)
+        J_history[i] = computeCost(X, y, theta)
 
     return theta, J_history
 
@@ -60,3 +76,7 @@ def featureNormalize(X):
         X_norm[:,i] = (X_norm[:,i] - mu[:,i])
         X_norm[:,i] = (X_norm[:,i] / sigma[:,i])
     return X_norm
+
+# alternative to gradient descent
+def normalEqn(X, y):
+    return np.linalg.inv(X.T * X) * X.T * y
