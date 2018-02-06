68687feeaef14573b49e9ae79d1871b6132531ee
unknown <caio.brig@gmail.com>
2018-02-06 11:22:05 -0500

    Finished PCA section of ex7

--- ex7/Octave/pca.m
+++ ex7/Octave/pca.m
@@ -14,16 +14,14 @@ S = zeros(n);
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should first compute the covariance matrix. Then, you
 %               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
+%               and eigenvalues of the covariance matrix.
 %
 % Note: When computing the covariance matrix, remember to divide by m (the
 %       number of examples).
 %
-
-
-
-
-
+covmatrix = (1/m) * (X' * X);
+size(covmatrix);
+[U, S, V] = svd(covmatrix);
 
 
 % =========================================================================
